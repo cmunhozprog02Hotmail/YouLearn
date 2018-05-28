978a3f3ea999fdd54e2e835becbf243cf480d0e0
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-05-28 20:02:01 -0300

    Aula 11 - Incompleta

---
+++ YouLearn/YouLearn.Domain/Services/ServiceUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouLearn.Domain.Arguments.Usuario;
+using YouLearn.Domain.Interfaces.Services;
+
+namespace YouLearn.Domain.Services
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        public AdicionarUsuarioResponse AdicionarUsusario(AdicionarUsuarioRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AutenticarUsuarioResponse AutenticarUsuario(AutenticarUsuarioRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
