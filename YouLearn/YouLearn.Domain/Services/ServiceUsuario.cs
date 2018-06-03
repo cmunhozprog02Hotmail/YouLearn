31cd2facc88bb6c08bd6ca7d3a5d1d2443f451bb
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-06-03 00:02:11 -0300

    Aula 12 parte 2

--- YouLearn/YouLearn.Domain/Services/ServiceUsuario.cs
+++ YouLearn/YouLearn.Domain/Services/ServiceUsuario.cs
@@ -1,8 +1,11 @@
 ﻿using prmToolkit.NotificationPattern;
+using prmToolkit.NotificationPattern.Extensions;
 using System;
 using YouLearn.Domain.Arguments.Usuario;
 using YouLearn.Domain.Entities;
 using YouLearn.Domain.Interfaces.Services;
+using YouLearn.Domain.Resourses;
+using YouLearn.Domain.ValueObjects;
 
 namespace YouLearn.Domain.Services
 {
@@ -12,15 +15,22 @@ namespace YouLearn.Domain.Services
         {
             if(request == null)
             {
-                AddNotification("AdicionarUsuarioRequest", "Objeto AdicionarUsuarioRequest é obrigatório.");
+                AddNotification("AdicionarUsuarioRequest", MSG.OBJETO_X0_E_OBRIGATORIO.ToFormat("AdicionarUsuarioRequest"));
                 return null;
             }
 
+
+            //Cria entitade
+
+            Nome nome = new Nome();
+            nome.PrimeiroNome = request.PrimeiroNome;
+            nome.UltimoNome = request.UltimoNome;
+
             Usuario usuario = new Usuario();
-            usuario.Nome.PrimeiroNome = "Paulo Rogèrio";
-            usuario.Nome.UltimoNome = "Martins Marques";
-            usuario.Email.Endereco = "paulo.analista@outlook.com";
-            usuario.Senha = "123456";
+            usuario.Nome = nome;
+            usuario.Nome.UltimoNome = request.UltimoNome;
+            usuario.Email.Endereco = request.Email;
+            usuario.Senha = request.Senha;
 
             //Persiste no Banco de Dados
             AdicionarUsuarioResponse response = new RepositoryUsuario(usuario);
