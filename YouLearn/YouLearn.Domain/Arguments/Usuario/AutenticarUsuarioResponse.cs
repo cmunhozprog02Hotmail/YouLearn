f2a814b0c4caec3b6132a8eaf62ee613b60b8602
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-06-13 10:24:30 -0300

    Aula 14 completa

--- YouLearn/YouLearn.Domain/Arguments/Usuario/AutenticarUsuarioResponse.cs
+++ YouLearn/YouLearn.Domain/Arguments/Usuario/AutenticarUsuarioResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using YouLearn.Domain.Entities;
 
 namespace YouLearn.Domain.Arguments.Usuario
 {
@@ -6,5 +7,14 @@ namespace YouLearn.Domain.Arguments.Usuario
     {
         public Guid Id { get; set; }
         public string PrimeiroNome { get; set; }
+
+        public static explicit operator AutenticarUsuarioResponse(Entities.Usuario entidade)
+        {
+            return new AutenticarUsuarioResponse()
+            {
+                Id = entidade.Id,
+                PrimeiroNome = entidade.Nome.PrimeiroNome
+            };
+        }
     }
 }
